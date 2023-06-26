bb75d348b0d3d5f846d46949ce1985e456fbe403
Arturo <arturo.masullo2003@icloud.com>
2023-06-26 14:17:10 +0200

    Added 5.5

---
+++ Esercizi booklet/Cap 5/5.5.py
@@ -0,0 +1,14 @@
+"""
+Write a program that displays how to count by 5 to 50. (using while loop)
+"""
+
+i = -10
+while i < 0:
+    print(i*-1*5)
+    i = i+1
+# Output in console più vecchi vengono messi sotto, il più recente è 50
+
+# Non si dovrebbero quasi mai usare i while loop essendo 1.667 volte più lenti dei for
+# Nonostante ciò per la quasi totalità delle applicazioni questa differenza di velocità non entra in gioco
+# Per informazioni aggiuntive:
+# https://www.youtube.com/watch?v=Qgevy75co8c&t=261s&ab_channel=mCoding
